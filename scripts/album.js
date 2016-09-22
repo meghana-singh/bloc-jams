77d0d31a4402ba371c49bf35eef90b30cf5ea357
meghana singh <meghana1602@gmail.com>
2016-09-22 11:29:43 -0700

    Assignment-20 : Function togglePlayFromPlayerBar added.

--- scripts/album.js
+++ scripts/album.js
@@ -180,6 +180,20 @@ var previousSong = function () {
     $lastSongNumberCell.html(lastSongNumber);    
 };
 
+//Play Pause the song from the player bar.
+var togglePlayFromPlayerBar = function () {
+    if (currentSoundFile.isPaused() == true) {
+        $(this).html(pauseButtonTemplate);  
+        $playPauseButton.html(playerBarPauseButton);
+        currentSoundFile.play();  
+    } else {
+        $(this).html(playButtonTemplate);
+        $playPauseButton.html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }
+};
+
+
 /* Set the currentlyPlayingSongNumber and set the currentSoundFile object */
 var setSong = function (songN) {
     //If a song is playing, stop it before you can play another song.
@@ -231,8 +245,9 @@ var currentSongFromAlbum = null;
 var currentSoundFile = null;
 var currentVolume = 80;
 
-var $previousButton = $('.main-controls .previous');
-var $nextButton = $('.main-controls .next');
+var $previousButton  = $('.main-controls .previous');
+var $nextButton      = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 /***************** Default album - Picasso ***********************/
 $(document).ready( function() {
@@ -252,6 +267,7 @@ $(document).ready( function() {
 
     $previousButton.click(previousSong);
     $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar);
     
 });
 
